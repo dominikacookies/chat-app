1cf80fc60afa8bde75dd637c3422846757c592e2
Dominika Pietrzak <dom.pietrzak@capgemini.com>
2022-02-08 14:23:15 +0000

    feature: logic for logging in users and creating new chat user via gmail

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+
+import { auth } from "../firebase";
+import { useAuth } from "../contexts/AuthContext";
+import axios from "axios";
 
 const Chats = () => {
-  return <h1>Chats</h1>;
+  const history = useHistory();
+  const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    history.push("/");
+  };
+
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
+  };
+
+  async function fetchData() {
+    try {
+      await axios.get("https://api.chatengine.io/users/me", {
+        headers: {
+          "project-id": process.env.REACT_APP_PROJECT_CHAT_ID,
+          "user-name": user.displayName,
+          "user-secret": user.uid,
+        },
+      });
+
+      setLoading(false);
+    } catch (error) {
+      let formData = new FormData();
+      formData.append("email", user.email);
+      formData.append("username", user.displayName);
+      formData.append("secret", user.uid);
+
+      const avatar = await getFile(user.photoURL);
+      formData.append("avatar", avatar, avatar.name);
+
+      try {
+        await axios.post("https://api.chatengine.io/users", formData, {
+          headers: { "private-key": process.env.REACT_APP_CHAT_PRIVATE_KEY },
+        });
+
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+      return;
+    }
+    fetchData();
+  }, [loading, user]);
+
+  return !loading && user ? (
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">Chat</div>
+        <div className="logout-tab" onClick={handleLogout}>
+          Logout
+        </div>
+      </div>
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectID={process.env.REACT_APP_PROJECT_CHAT_ID}
+        userName={user.displayName}
+        userSecret={user.uid}
+      />
+    </div>
+  ) : (
+    <h1> Sit tight, we're loading your information.</h1>
+  );
 };
 
 export default Chats;
--- src/components/Login.js
+++ src/components/Login.js
@@ -18,14 +18,6 @@ const Login = () => {
           <GoogleOutlined /> Sign in with Google
         </div>
         <br /> <br />
-        <div
-          className="login-button facebook"
-          onClick={() =>
-            auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())
-          }
-        >
-          <FacebookOutlined /> Sign in with Facebook
-        </div>
       </div>
     </div>
   );
