b7f07038c7d146f7c439f23cbdb7556a1b4b19cb
Dominika Pietrzak <dom.pietrzak@capgemini.com>
2022-02-10 09:52:18 +0000

    feat: component for standardization of form inputs

---
+++ src/components/FormInput.js
@@ -0,0 +1,34 @@
+import React from "react";
+import classNames from "classnames";
+
+const FormInput = ({
+  placeholder,
+  additionalClassNames,
+  type = "text",
+  error,
+  register,
+  required,
+  onBlur,
+  id,
+  value,
+}) => {
+  return (
+    <div className="form-element">
+      <input
+        type={type}
+        className={classNames("form-input", {
+          "form-input--error": error,
+          additionalClassNames,
+        })}
+        placeholder={placeholder}
+        defaultValue={value}
+        {...register}
+        required={required}
+        onBlur={onBlur}
+        id={id}
+      />
+    </div>
+  );
+};
+
+export default FormInput;
